0e816bf0b146543be18608ca81be3de98d4cf4c2
Mthokoisi_tk2 <mthokozisi.gofourit@gmail.com>
2025-06-10 16:25:26 +0200

    Make changes to Advance.sh

--- Advance.sh
+++ Advance.sh
@@ -17,39 +17,56 @@ age=1652
 greeting="Guten Tag"
 
 
-#2. Echo (Output)
+
+
+echo ""
+echo "#2. Echo (Output)"
+echo ""
+
 echo "$greeting, $name! You are $age years old."
 
 
 #3. Read User Input
 read -p "Enter your favourite Langauge for Scripting: " LANGUAGE
-echo "You Like: $LANGUAGE!"
+echo "Your chosen Language is: $LANGUAGE!"
 
 
-echo " "
+echo ""
 echo "***************"
 echo " "
-sleep 1
+sleep 2
 
 
-#4. If-else Statements
+
+
+
+echo ""
+echo "#4. If-else Statements"
+echo ""
+
+
 if [ "$LANGUAGE" == "bash" ]; then
-  echo "Bash is Powerful."
+  echo "Nice, Bash is Powerful."
 elif [ "$LANGUAGE" == "python" ]; then
-  echo "Python is Versatile."
+  echo "Cool, Python is Versatile."
 else
-  echo "That $LANGUAGE is cool too"
+  echo "That language is Cool Too"
 fi
 
 
-
-echo " "
+echo ""
 echo "***************"
 echo " "
-sleep 1
+sleep 3
 
 
-#5. Case statement
+
+
+
+echo ""
+echo "#5. Case statement"
+echo ""
+
 read -p "Enter a number from 1-3: " NUM
 
 case $NUM in
@@ -60,89 +77,122 @@ case $NUM in
 esac
 
 
-
-echo " "
+echo ""
 echo "******************"
 echo " "
-sleep 1
+sleep 4
 
 
-#6. For Loop
-echo "Counting from 1 -> 3 using for Loop:"
+
+
+
+echo ""
+echo "#6. For Loop"
+echo ""
+
+
+echo "Counting from 1 -> 3 using For Loop:"
 
 for i in {1..3}; do
-  echo "i: $i"
+  echo "i. $i"
 done
 
-
-echo " "
+echo ""
 echo "********************"
 echo " "
-sleep 1
+sleep 4
 
 
-#7. While Loop
+
+
+
+echo ""
+echo "#7. While Loop"
+echo ""
 
 count=3
 echo "Countdown using While Loop "
+
 while [ $count -gt 0 ]; do
   echo "Countdown: $count"
   ((count--))
 done
 
-echo " "
+echo ""
 echo "******************"
 echo " "
-sleep 1
+sleep 4
 
 
-#8. Function
+
+
+echo ""
+echo "#8. Function"
+echo ""
+
 greet_user(){
-  echo "Inside Function: Hello again, $1!"
+  echo "Printing From Inside The Function: Hello again, $1!"
 }
 greet_user "$name"
 
-
-echo " "
+echo ""
 echo "************************"
 echo " "
-sleep 1
+sleep 4
 
 
 
-#9. Arrays
-colors=("red", "blue", "green")
-echo "Array items: "
+
+echo ""
+echo "#9. Arrays"
+echo ""
+
+colors=("red", "blue", "green", "orange", "yellow")
+
+echo "Array Items: "
+
 for color in "${colors[@]}"; do
   echo "$color"
 done
 
-echo "First color is: ${colors[0]}"
+echo ""
+echo "Third color in Array is: ${colors[2]}"
 
 
 echo ""
 echo "*********************"
 echo ""
-sleep 2
+sleep 4
 
 
 
 
-#10. Command-Line Arguements
+
+
+echo ""
+echo "#10. Command-Line Arguements"
+echo ""
+
 echo "You passed $# arguments: $@"
 echo "First arg is: $1"
 echo "Second arg is: $2"
 
 
-echo " "
+echo ""
 echo "*****************"
-echo " "
-sleep 2
+echo ""
+sleep 4
 
 
 
 
-#11. getopts (flags parsing)
+
+
+echo ""
+echo "#11. getopts (flags parsing)"
+echo "" 
+
+
 while getopts ":u:p:" opt; do
   case $opt in
     u) user="$OPTARG" ;;
@@ -151,18 +201,28 @@ while getopts ":u:p:" opt; do
   esac
 done
 
+
 if [[ -n "$user" && -n "$pass" ]]; then
-  echo "User: $user, Pass: $pass
+  echo "User: $user, Pass: $pass"
+else 
+  echo "GeoTopts Not Working"
 fi
 
-echo " "
+
+echo ""
 echo "************************"
 echo " "
-sleep 1
+sleep 5
 
 
 
-#12. Arithmetic
+
+
+echo ""
+echo "#12. Arithmetic"
+echo ""
+
+
 a=5
 b=3
 sum=$(( a+b ))
@@ -173,37 +233,24 @@ echo "$a + $b = $sum"
 echo ""
 echo "*******************"
 echo ""
-sleep 2
+sleep 3
 
 
 
-#13. Command substitution
+
+
+
+
+
+echo ""
+echo "#13. Command substitution"
+echo ""
+
+
 today=$(date "+%Y-%m-%d")
 echo "Today's date is $today"
 
 
-
-echo ""
-echo "******************"
-echo ""
-sleep 2
-
-
-
-#14. logging
-
-
-echo ""
-echo"********************"
-echo ""
-sleep 2
-
-
-
-
-
-#15 File redirection
-
 echo ""
 echo "******************"
 echo ""
@@ -212,29 +259,95 @@ sleep 2
 
 
 
-#16. Exit status
+
+
+
+echo ""
+echo "#14. logging"
+echo ""
+
+log_file="script.log"
+echo "[$today] Script executed by $USER" >> "$log_file"
+
+
+echo ""
+echo "********************"
+echo ""
+sleep 2
+
+
+
+
+
+
+
+
+
+echo ""
+echo "#15 File redirection"
+echo ""
+
+echo "This will overwrite file.txt" > file.txt
+echo "This will append to file.txt" >> file.txt
+
+
+echo ""
+echo "******************"
+echo ""
+sleep 4
+
+
+
+
+
+
+
+
+
+
+
+echo ""
+echo "#16. Exit status"
+echo ""
+
 
 
 echo ""
 echo "*****************"
 echo ""
+sleep 4
+
+
+
+
+
+
+
+
+
+
+
+echo ""
+echo "#17. Trap & Signal Handliing"
+echo ""
+
+
+echo ""
+echo "*********************"
+echo ""
 sleep 2
 
 
 
 
 
-#17. Trap & Signal Handliing
+
+
 
 echo ""
-echo"*********************"
+echo "#18.  Subshell"
 echo ""
-sleep 2
 
-
-
-
-#18.  Subshell
 
 echo ""
 echo "****************"
@@ -246,16 +359,17 @@ sleep 2
 
 
 
+echo ""
+echo "#19. Checking if a file exists"
+echo ""
 
-#19. Checking if a file exists
 file="myfile.txt"
 if [ -f "file" ]; then
   echo "File '$file' exists."
-else 
-  echo "File '$file' does not existCreating it..."
+else
+  echo "File '$file' does not exist. Creating it..."
   echo "This is a simple file." > "$file"
 fi
-
 
 echo ""
 echo "*****************"
