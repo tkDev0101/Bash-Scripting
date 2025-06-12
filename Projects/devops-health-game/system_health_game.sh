953197602d2521b6845ebb98a2b7c82686e10eff
Mthokoisi_tk2 <mthokozisi.gofourit@gmail.com>
2025-06-12 15:02:10 +0200

    Add save report to log feature

--- Projects/devops-health-game/system_health_game.sh
+++ Projects/devops-health-game/system_health_game.sh
@@ -61,16 +61,116 @@ show_top_proceses(){
 
 
 #3. Fuction: Full Health Check Report (to Screen Only)
+run_full_health_report(){
+
+  echo -e "\n${YELLOW}==== System Health Check ====${NC}"
+
+
+  echo -e "\n${YELLOW}--- Memory Usage ---${NC}"
+  free -h
+
+
+  echo -e "\n${YELLOW}--- Disk Usage ---${NC}"
+  df -h | grep '^/dev/'
+
+
+  echo -e "\n${YELLOW}--- System Uptime ---${NC}"
+  uptime -p
+
+
+  echo -e "\n${yellow}---- Top 5 Processess by CPU Usage ----${NC}"
+  ps -eo pid,user,%cpu,comm --sort=-%cpu | head -n 6 | tail -n 5 | while read pid user cpu command; do
+	printf "%-8s %-10s %-8s %s\n" "$pid" "$user" "$cpu" "command"
+  done
+
+
+  echo -e "\n${YELLOW}--- Disk Space Alerts (10%) ---${NC}"
+  df -h --output=pcent,target | tail -n +2 | while read output; do
+    USEP=$(echo "$output" | awk '{print $1}' | tr -d '%')
+    PARTITION=$( echo "$output" | awk '{print $2}' )
+    if [ "$USEP" -ge 10 ]; then
+	echo -e "${RED}Warning:${NC} Partition ${PARTITION} is ${USEP}% full."
+    fi
+  done
+
+
+  echo -e "\n${GREEN}>> Health Check Complete.${NC}\n"
+
+ }
+
+
+
+#4. Fuction: Save Full Health Report to Log file
+save_report_to_log(){
+
+  #Define Log Directory & Filename
+  LOG_DIR=~/Shell-Scripting-WSL/Projects/devops-health-game/logs
+
+  TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+  LOG_FILE="$LOG_DIR/system_health_$TIMESTAMP.log"
+
+
+  #Ensure the Log directory exists
+  mkdir -p "$LOG_DIR"
+
+
+  #Begin log
+  {
+	echo "====================================="
+	echo "==== System Health Report: $TIMESTAMP ====="
+	echo "====================================="
+
+
+	echo -e "\n--- Memory Usage ---"
+	free -h
+
+
+	echo -e "\n--- Disk Usage ---"
+	df -h | grep '^/dev/'
+
+
+	echo -e "\n--- System Uptime ---"
+	uptime -p
+
+
+	echo -e "\n---- Top 5 Processess by CPU ----"
+	ps -eo pid,user,%cpu,comm --sort=-%cpu | head -n 6 | tail -n 5 | while read pid user cpu command; do
+	    printf "%-8s %-10s %-8s %s\n" "$pid" "$user" "$cpu" "command"
+	done
+
+
+	echo -e "\n--- Disk Space Alerts (10%) ---"
+
+	df -h --output=pcent,target | tail -n +2 | while read output; do
+	  USEP=$(echo "$output" | awk '{print $1}' | tr -d '%')
+	  PARTITION=$( echo "$output" | awk '{print $2}' )
+	  if [ "$USEP" -ge 10 ]; then
+	      echo -e "Warning: Partition ${PARTITION} is ${USEP}% Full."
+	  fi
+	done
+
+
+	echo "==== END OF REPORT====="
+  } >> "$LOG_FILE"
+
+
+
+  echo -e "\n${GREEN}>> Report Save to: ${LOG_FILE}${NC}\n"
+
+ }
+#end function
 
 
 
 
+
+
+#==========================================================
 
 # Control shift 6	   -> set mark
 # Alt 6			   -> Copy
 # Cotrol k		   -> Cut
 # Control U		   -> Paste
-
 
 
 #========================================================
@@ -107,11 +207,11 @@ while true; do
 	;;
    3)
 	echo -e "\n${GREEN}>> Running Full Health Check...${NC}"
-	#
+	run_full_health_report
 	;;
    4)
 	echo -e "\n${GREEN}>> Saving Report to $LOG_FILE...${NC}"
-	#
+	save_report_to_log
 	;;
    5)
 	echo -e "\n${YELLOW}Exiting Mission Health Check. Goodbye.${NC}"
