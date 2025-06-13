11b3bca9ca2a5af04077667c53c577b28291ca7a
tkDev <tkdev0101@gmail.com>
2025-06-13 06:20:42 +0200

    Add Health Log View

--- Projects/devops-health-game/system_health_game.sh
+++ Projects/devops-health-game/system_health_game.sh
@@ -45,6 +45,8 @@ view_system_status() {
 
 
 
+
+
 #2. Function:  Display Top 5 CPU Processes
 show_top_proceses(){
     echo -e "\n${yellow}---- Top 5 Processess by CPU Usage ----${NC}"
@@ -57,6 +59,8 @@ show_top_proceses(){
 
     echo ""
  }
+
+
 
 
 
@@ -97,6 +101,9 @@ run_full_health_report(){
   echo -e "\n${GREEN}>> Health Check Complete.${NC}\n"
 
  }
+
+
+
 
 
 
@@ -152,13 +159,67 @@ save_report_to_log(){
 
 	echo -e "\n\n==== END OF REPORT=====\n\n"
   } >> "$LOG_FILE"
-
+  #end log
 
 
   echo -e "\n${GREEN}>> Report Save to: ${LOG_FILE}${NC}\n"
 
  }
 #end function
+
+
+
+
+
+
+
+#5. Fuction: View  Previous Reports
+view_previous_reports() {
+
+
+  #Define Log Directory & Filename
+  LOG_DIR=~/Shell-Scripting-WSL/Projects/devops-health-game/logs
+
+  #Check if any logs exist
+  if [ ! -d "$LOG_DIR" ] || [ -z "$(ls -A "$LOG_DIR")" ]; then
+    echo -e "\n${RED}No Reports found. Run option 4 to generate logs first.${NC}\n"
+    return
+  fi
+
+  echo -e "\n${YELLOW}Avaliable Reports: ${NC}"
+  ls "$LOG_DIR" | nl
+
+
+  echo -e "\n${CYAN}Enter the Number of the Report you want to view: ${NC}"
+  read -rp ">>" choice
+
+  selected_file=$( ls "$LOG_DIR" | sed -n "${choice}p" )
+  full_path="$LOG_DIR/$selected_file"
+
+  if [ -f "$full_path" ]; then
+    echo -e "\n${GREEN}Displaying: $selected_file${NC}\n"
+    cat "$full_path"
+  else
+    echo -e "\n${RED}Invalid Selection.${NC}"
+  fi
+
+
+ }
+ #end Function
+
+
+
+
+
+
+
+#6. Function - Disk Sapce alerts
+check_disk_alerts() {
+
+  echo 
+
+ }
+ #end function
 
 
 
@@ -192,7 +253,9 @@ while true; do
   echo "2. View Top 5 Processes"
   echo "3. Run Full Health Check Report"
   echo "4. Save Report to Log File"
-  echo -e "5. Exit\n"
+  echo "5. View Previous Health Reports"
+  echo "6. Check Disk Space Alerts"
+  echo -e "7. Exit\n"
   read -p "Choose an Option [1-5]: " choice
 
 
@@ -213,7 +276,18 @@ while true; do
 	echo -e "\n${GREEN}>> Saving Report to $LOG_FILE...${NC}"
 	save_report_to_log
 	;;
+
    5)
+	echo -e "\n${GREEN}>> Viewing Previous Health Reports...${NC}"
+	view_previous_reports
+	;;
+
+   6)
+	echo -e "\n${GREEN}>> Running Disk Space Alert Check...${NC}"
+	check_disk_alerts
+	;;
+
+   7)
 	echo -e "\n${YELLOW}Exiting Mission Health Check. Goodbye.${NC}"
 	break
 	;;
